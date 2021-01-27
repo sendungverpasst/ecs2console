bbb03e709cf4e0b81e5b9d61238ee918f89cd645
Florian Pahl <fpahl@gmx.de>
2021-01-27 14:24:28 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,22 @@
+# ecs2console
+
+Convert logs in ECS format to console readable output. 
+
+## Install
+
+``` bash
+npm install
+npm link
+```
+
+On MacOS the folder  `/usr/local` might not be writeable for the current user. You ccan change this by executing:
+``` bash
+sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+```
+
+## Test
+
+See if it works by running:
+``` bash
+more test/log.json | ecs2console
+```
---
+++ bin/ecs2console.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(line){
+    console.log(convert(line));
+})
+
+function convert(line) {
+  try {
+    const json = JSON.parse(line)
+  
+    const level = formatLevel(json.log.level)
+    const timestamp = formatTimestamp(json['@timestamp'])
+    const message = json.message
+  
+    return `${timestamp} - ${level}: ${message}`
+  } catch (err) {
+    return line
+  }
+}
+
+function formatTimestamp(timestamp) {
+  const date = timestamp.substring(0, 10)
+  const time = timestamp.substring(11, 19)
+  return `${date} ${time}`
+}
+
+function formatLevel(level) {
+  let formatted = level.toUpperCase()
+  formatted += '  '
+  formatted = formatted.substring(0, 5) 
+  return formatted
+}
