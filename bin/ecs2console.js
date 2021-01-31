84b4734f41b9e876f68c83f10babcf4c38984daa
Florian Pahl <fpahl@gmx.de>
2021-01-31 19:39:40 +0100

    colorize output with chalk

--- bin/ecs2console.js
+++ bin/ecs2console.js
@@ -3,14 +3,34 @@
 const readline = require('readline')
 const commander = require('commander')
 const program = new commander.Command()
+const chalk = require('chalk')
+const format = require('date-fns/format')
 
 const levels = {
-  fatal: 1,
-  error: 2,
-  warn: 3,
-  info: 4,
-  debug: 5,
-  trace: 6
+  fatal: {
+    number: 1,
+    color: chalk.bgRed
+  },
+  error: {
+    number: 2,
+    color: chalk.red
+  },
+  warn: {
+    number: 3,
+    color: chalk.yellow
+  },
+  info: {
+    number: 4,
+    color: chalk.green
+  },
+  debug: {
+    number: 5,
+    color: chalk.blue
+  },
+  trace: {
+    number: 6,
+    color: chalk.gray
+  }
 }
 
 program.version('1.0.0')
@@ -21,7 +41,7 @@ program.parse(process.argv)
 
 const options = program.opts()
 const level = options.level || 'trace'
-const levelNr = levels[level]
+const levelNr = levels[level].number
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -42,12 +62,12 @@ function convert (line, levelNr) {
     const level = json.log.level
 
     // filter by log level
-    const loggedLevelNr = levels[level.toLowerCase()]
+    const loggedLevelNr = levels[level.toLowerCase()].number
     if (loggedLevelNr && loggedLevelNr > levelNr) return
 
     const levelFormatted = formatLevel(level)
     const timestamp = formatTimestamp(json['@timestamp'])
-    const message = json.message
+    const message = formatMessage(json.message)
 
     return `${timestamp} - ${levelFormatted}: ${message}`
   } catch (err) {
@@ -55,15 +75,20 @@ function convert (line, levelNr) {
   }
 }
 
+function formatMessage (message) {
+  return chalk.cyan(message)
+}
+
 function formatTimestamp (timestamp) {
-  const date = timestamp.substring(0, 10)
-  const time = timestamp.substring(11, 19)
-  return `${date} ${time}`
+  const date = new Date(timestamp)
+  return format(date, 'HH:mm:ss')
 }
 
 function formatLevel (level) {
   let formatted = level.toUpperCase()
   formatted += '  '
   formatted = formatted.substring(0, 5)
-  return formatted
+
+  const color = levels[level].color
+  return color(formatted)
 }
