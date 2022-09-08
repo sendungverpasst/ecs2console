893194d49b5bdc33bcedf5b724c81257d342d99d
Florian Pahl <fpahl@gmx.de>
2022-09-08 21:58:41 +0200

    log exception

--- bin/ecs2console.js
+++ bin/ecs2console.js
@@ -57,28 +57,44 @@ rl.on('line', function (line) {
 })
 
 function convert (line, levelNr) {
+  let json = null
   try {
-    const json = JSON.parse(line)
+    json = JSON.parse(line)
+  } catch (e) {
+    // not a json
+    return line
+  }
+
+  try {
     // different output from ecs-pino-format Version 0.2.0 vs 0.1.0
     const level = json['log.level'] || json.log.level
+    const cleanLevel = level === 'WARNING' ? 'warn' : level.toLowerCase()
 
     // filter by log level
-    const loggedLevelNr = levels[level.toLowerCase()].number
+    const loggedLevelNr = levels[cleanLevel.toLowerCase()].number
     if (loggedLevelNr && loggedLevelNr > levelNr) return
 
-    const levelFormatted = formatLevel(level)
+    const levelFormatted = formatLevel(cleanLevel)
     const timestamp = formatTimestamp(json['@timestamp'])
-    const message = formatMessage(json.message)
+    const message = formatMessage(json)
+    const errStack = formatException(json)
 
-    return `${timestamp} - ${levelFormatted}: ${message}`
+    return `${timestamp} - ${levelFormatted}: ${message}${errStack}`
   } catch (err) {
-    // console.log(err)
+    console.error(`ERROR: Unexpected bug in ecs2sonosle - ${err}`)
     return line
   }
 }
 
-function formatMessage (message) {
-  return chalk.cyan(message)
+function formatMessage (json) {
+  let message = json.message
+  if (json.scraper) {
+    const scraperDetails = []
+    Object.entries(json.scraper).map(([key, val]) => key !== 'exception' && scraperDetails.push(`${key}=${JSON.stringify(val)}`))
+    if (scraperDetails.length) message += ` (${scraperDetails.join(', ')})`
+  }
+
+  return chalk.cyan(`${message}`)
 }
 
 function formatTimestamp (timestamp) {
@@ -96,5 +112,39 @@ function formatLevel (level) {
     return color(formatted)
   } else {
     return formatted
+  }
+}
+
+function formatException (json) {
+  let exception = null
+  try {
+    exception = json.stack || json.scraper.exception
+  } catch (e) {}
+
+  if (exception) {
+    let errStack = ''
+
+    // Parse error.type and error.details from ApifyClientError.
+    const errDetails = []
+    if (exception.type) errDetails.push(`type=${exception.type}`)
+    if (exception.details) {
+      Object.entries(exception.details).map(([key, val]) => errDetails.push(`${key}=${val}`))
+    }
+
+    // Parse error stack lines.
+    // NOTE: Reason is here to support Meteor.js like errors.
+    const errorString = exception.stack || exception.reason || exception.toString()
+    const errorLines = errorString.split('\n')
+
+    // Add details to a first line.
+    if (errDetails.length) errorLines[0] += `(details: ${errDetails.join(', ')})`
+
+    // Compose it back.
+    errStack = errorLines.map((line) => `  ${line}`).join('\n')
+    errStack = chalk.grey(`\n${errStack}`)
+
+    return errStack
+  } else {
+    return ''
   }
 }
